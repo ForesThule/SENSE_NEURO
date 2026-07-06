7bca81421285a5aa3263067073fb0e2a73e15ad4
ve <lesthule@gmail.com>
2026-07-06 22:40:03 +0300

    Revert "multi-band mode: one process per band, 3 generated scripts"
    This reverts commit 9ed511a4ede9e0e59122893aa406616b219f23aa.

--- START_NEIRY_BAND_1.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title NEIRY_BAND_1_R
-cd /d "C:\SENSE_TECH"
-set "NEIRY_HEAD=192.168.1.34"
-rem бенд/порты вшиты в neiry_band_1.py (F7:14:0E:FE:9D:20 -> 9003/9013, стена R)
-:loop
-python "C:\SENSE_TECH\neiry_band_1.py"
-timeout /t 3 /nobreak >nul
-goto loop
--- START_NEIRY_BAND_3.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title NEIRY_BAND_3_L
-cd /d "C:\SENSE_TECH"
-set "NEIRY_HEAD=192.168.1.34"
-rem бенд/порты вшиты в neiry_band_3.py (EF:21:DE:1C:A3:67 -> 9001/9011, стена L)
-:loop
-python "C:\SENSE_TECH\neiry_band_3.py"
-timeout /t 3 /nobreak >nul
-goto loop
